46e53dd519a18924b43c6de8c46e797070fb25d9
Miguel Machado <hello@miguelmachado.dev>
2025-10-06 18:04:05 -0300

    feat: update job query to return a list of jobs with required fields

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,11 +1,18 @@
 export const resolvers = {
   Query: {
-    job: () => {
-      return {
-        id: "test-id",
-        title: "The title",
-        description: "The description",
-      };
+    jobs: () => {
+      return [
+        {
+          id: "test-id-1",
+          title: "The title 1",
+          description: "The description",
+        },
+        {
+          id: "test-id-2",
+          title: "The title 2",
+          description: "The description",
+        },
+      ];
     },
   },
 };
