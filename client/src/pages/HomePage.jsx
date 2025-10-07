ce3f8765aac76bcc21bfe1afe24e3c57da20ff6e
Miguel Machado <hello@miguelmachado.dev>
2025-10-07 15:25:06 -0300

    feat: implement getJobs function to fetch job data from GraphQL API

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,12 +1,12 @@
-import JobList from '../components/JobList';
-import { jobs } from '../lib/fake-data';
+import JobList from "../components/JobList";
+import { getJobs } from "../lib/graphql/queries";
+
+const jobs = getJobs().then((jobs) => console.log({ jobs }));
 
 function HomePage() {
   return (
     <div>
-      <h1 className="title">
-        Job Board
-      </h1>
+      <h1 className="title">Job Board</h1>
       <JobList jobs={jobs} />
     </div>
   );
